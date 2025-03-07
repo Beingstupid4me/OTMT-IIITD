30853a7fccc04ad52286024fd5ccbac507979a99
Amartya Singh <119071717+Beingstupid4me@users.noreply.github.com>
2025-03-07 14:31:21 +0530

    Version 2

--- src/App.js
+++ src/App.js
@@ -1,65 +1,62 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import './App.css';
-import Home from "./pages/Home/Home";
-import OurServices from "./pages/Our_services/OurServices";
-import FacInn from "./pages/Facillitate_innovation/FacInn";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Tam from "./pages/Tech_assessment/Tam";
-import Tech from "./pages/Technology/Tech";
-import Res from "./pages/Resources/Res";
-import Coll from "./pages/Collaborate/Coll";
-import Eve from "./pages/Events/Eve";
-import Cont from "./pages/Contact/Cont";
-import Lisc from "./pages/Liscensing/Lisc";
-import IPR_man from "./pages/IPR/IPR_man";
-import Startup from "./pages/Startup_new/Start";
-import Tech_res from "./pages/Tech_Research/Tech_res";
-import Tech_tech from "./pages/Tech_Technology/Tech_tech";
-import Fac_Res from "./pages/Fac_Resources/Fac_Res";
-import Ipr_Table from "./pages/IPR_TABLE/Ipr_Table";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SiteHeader from './components/header/component';
+import SiteFooter from './components/footer/component';
+import './App.css';
 
-import Up_eve from "./pages/Up_Events/Up_eve";
-import All_tech from "./pages/All_Techno/All_tech";
-import Stu_Res from "./pages/Student_Resources/Stu_Res";
-import Par_Res from "./pages/Partner_Resources/Par_Res";
-import Past from "./pages/Past_events/Past";
-import Proj from "./pages/Spon_Projects/Proj";
-
+// Import your page components as needed
+import HomePage from './pages/page';
+import CollaboratePage from './pages/Collaborate/page';
+import EventPage from './pages/Event/page';
+import ContactPage from './pages/Contact_Us/page';
+import FacilitateInnovationPage from './pages/Our_Services/Facilitate_Innovation/page';
+import TechAssessmentPage from './pages/Our_Services/Tech_Assessment/page';
+import IPRManagementPage from './pages/Our_Services/IPR_Management/page';
+import TechLicensingPage from './pages/Our_Services/Tech_Licensing/page';
+import StartupFacilitationPage from './pages/Our_Services/Startup_Facilitation/page';
+import FacultyResourcesPage from './pages/Resources/Faculty/page';
+import PartnerResourcesPage from './pages/Resources/Partner/page';
+import StudentResourcesPage from './pages/Resources/Student/page';
+import OurResearchPage from './pages/Technology/Our_research/page';
+import OurTechPage from './pages/Technology/Our_tech/page';
+import ExploreTechnologiesPage from './pages/Technology/Explore/page';
+import TechDetail from './pages/Technology/TechDetail/TechDetail';
 
 function App() {
- 
+
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-       
+      <Router>
+        {/* Global header appears on every page */}
+        <SiteHeader />
         
-        <Route path='/' element={<Home />}/> 
-        <Route path="/service" element={<OurServices />} />
-        <Route path="/facinn" element={<FacInn />} />
-        <Route path="/tam" element={<Tam />} />
-        <Route path="/technology" element={<Tech />} />
-        <Route path="/resources" element={<Res />} />
-        <Route path="/collaborate" element={<Coll />} />
-        <Route path="/events" element={<Eve />} />
-        <Route path="/contact" element={<Cont />} />
-        <Route path="/Lisc" element={<Lisc />} />
-        <Route path="/Ipr" element={<IPR_man />} />
-        <Route path="/Startup" element={<Startup />} />
-        <Route path="/Tech_res" element={<Tech_res />} />
-        <Route path="/Tech_tech" element={<Tech_tech />} />
-        <Route path="/Fac_Res" element={<Fac_Res />} />
-        <Route path="/Ipr_Table" element={<Ipr_Table />} />
-        <Route path="/all_tech" element={<All_tech />} />
-        <Route path="/up_eve" element={<Up_eve />} />
-        <Route path="/stu_res" element={<Stu_Res />} />
-        <Route path="/par_res" element={<Par_Res />} />
-        <Route path="/Past" element={<Past />} />
-        <Route path="/Proj" element={<Proj />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+        {/* Define your routes here */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Collaborate" element={<CollaboratePage />} />
+          <Route path="/Event" element={<EventPage />} />
+          <Route path="/Contact_Us" element={<ContactPage />} />
+
+          {/* Services routes */}
+          <Route path="/Services/Facilitate_Innovation" element={<FacilitateInnovationPage />} />
+          <Route path="/Services/Tech_Assessment" element={<TechAssessmentPage />} />
+          <Route path="/Services/IPR_Management" element={<IPRManagementPage />} />
+          <Route path="/Services/Tech_Licensing" element={<TechLicensingPage />} />
+          <Route path="/Services/Startup_Facilitation" element={<StartupFacilitationPage />} />
+
+          {/* Resources routes */}
+          <Route path="/Resources/Faculty" element={<FacultyResourcesPage />} />
+          <Route path="/Resources/Partner" element={<PartnerResourcesPage />} />
+          <Route path="/Resources/Student" element={<StudentResourcesPage />} />
+
+          {/* Technology routes */}
+          <Route path="/Our_Research" element={<OurResearchPage />} />
+          <Route path="/Our_Technology" element={<OurTechPage />} />
+          <Route path="/Explore_Technologies" element={<ExploreTechnologiesPage />} />
+          <Route path="/tech/:id" element={<TechDetail />} />
+        </Routes>
+
+        {/* Global footer appears on every page */}
+        <SiteFooter />
+      </Router>
   );
 }
 
